4f96c29178e084e9944e8e423e99d1f046ceba1d
Patro <Sridhar.Patro@Dell.com>
2022-04-14 13:12:06 +0530

    Added File

--- SystrayPOC/Class1.cs
+++ SystrayPOC/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SystrayPOC
 {
-    internal class Class2
+    internal class Class1
     {
     }
 }
--- SystrayPOC/Class3.cs
+++ SystrayPOC/Class3.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SystrayPOC
 {
-    internal class Class1
+    internal class Class3
     {
     }
 }
