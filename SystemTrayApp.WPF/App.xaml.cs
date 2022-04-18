3a67de3e7d8c31fc885541d262094e3c9b250879
Patro <Sridhar.Patro@Dell.com>
2022-04-18 17:31:12 +0530

    new ckeckin

--- SystemTrayApp.WPF/App.xaml.cs
+++ SystemTrayApp.WPF/App.xaml.cs
@@ -6,12 +6,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SystrayPOC
+namespace SystemTrayApp.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+        }
     }
 }
--- SystemTrayApp.WPF/MainWindow.xaml.cs
+++ SystemTrayApp.WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SystrayPOC
+namespace SystemTrayApp.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
